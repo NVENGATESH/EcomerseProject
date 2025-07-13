bd7d2d0843d3858ce82f1015f9ad4d77bfbb03f0
NVENGATESH <vengatesh122003@gmail.com>
2025-07-13 09:28:39 +0530

    updated all

--- src/main/java/com/ecommerce/project/config/AppConfig.java
+++ src/main/java/com/ecommerce/project/config/AppConfig.java
@@ -4,6 +4,8 @@ package com.ecommerce.project.config;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 
 @Configuration
 public class AppConfig {
@@ -12,4 +14,12 @@ public class AppConfig {
     public ModelMapper modelMapper(){
         return new ModelMapper();
     }
+
+    
+
+    @Bean
+    public ObjectMapper objectMapper() {
+        return new ObjectMapper();
+    }
+
 }
